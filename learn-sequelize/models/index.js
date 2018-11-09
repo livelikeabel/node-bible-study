66797a68ea1d73de04f83ada79320f6b395df125
livelikeabel <esung1129@gmail.com>
2018-11-09 20:10:22 +0900

    define relationship

--- learn-sequelize/models/index.js
+++ learn-sequelize/models/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const path = require('path');
 const Sequelize = require('sequelize');
 
@@ -14,5 +12,8 @@ db.Sequelize = Sequelize;
 
 db.User = require('./user')(sequelize, Sequelize);
 db.Comment = require('./comment')(sequelize, Sequelize);
+
+db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
+db.Comment.belongsTo(db.User, { foreignKey: 'commenter', targetKey: 'id' });
 
 module.exports = db;
